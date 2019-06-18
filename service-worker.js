9abb83079e833fca2f568f55085fd1c9f193b1e1
Chris Underhill <cpu22@cam.ac.uk>
2019-06-18 15:48:35 +0100

    Make paths relative

--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,16 @@
 const CACHE_NAME = 'static-cache-v1';
 
 const FILES_TO_CACHE = [
-    '/index.html', 'main.css', 'sunPosition.js'
-];
+    'index.html', 'main.css', 'sunPosition.js'
+].map(x => location.pathname + x);
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log('[ServiceWorker] Pre-caching offline page');
-    return cache.addAll(FILES_TO_CACHE);
-    })
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
     self.skipWaiting();
 });
