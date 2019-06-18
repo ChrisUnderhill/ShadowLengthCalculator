26806580f745f4bc10db8fb8822a8e31a7a022ed
Chris Underhill <cpu22@cam.ac.uk>
2019-06-18 13:51:26 +0100

    Refresh every second

--- SunPosition.js
+++ SunPosition.js
@@ -22,49 +22,67 @@ function rightAscensionFromRad(rad)
     return ph.toString() + "h " + pm.toString() + "m " + ps.toString() + "s";
 }
 
-var today = Date.now();
 
-//today = Date.UTC(2019,06,16,16);
+var refreshPeriod = 1000;
 
-//30 days between 1970 and 2000
-var daysJ2000 = (today / 1000/365.25/24/3600 - 30) * 365.25;
+var timer = setInterval(myOnLoad, refreshPeriod);
 
-console.log(daysJ2000);
-
-var sunLongitude = 280.461 + 0.9856474*daysJ2000;
-
-sunLongitude = degToRad(sunLongitude % 360);
-
-var anomaly = 357.528 + 0.9856003 * daysJ2000;
-
-anomaly = degToRad(anomaly % 360);
-
-var eclipticLongitude = sunLongitude + degToRad(1.915 * Math.sin(anomaly) + 0.020 * Math.sin(2*anomaly));
-
-var eclipticObliquity = degToRad(23.439 - 0.0000004*daysJ2000);
-
-var y = Math.cos(eclipticObliquity) * Math.sin(eclipticLongitude);
-
-var x = Math.cos(eclipticLongitude);
-
-var RA = Math.atan2(y,x);
-
-var printRA = rightAscensionFromRad(RA);
-
-var DEC = radToDeg(Math.asin(Math.sin(eclipticObliquity) * Math.sin(eclipticLongitude)).toFixed(3));
-
-console.log(printRA, DEC);
+var loc = -1;
 
 function getLocation(callback) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(callback);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
+    if (loc === -1) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(x => {loc=x;});
+            navigator.geolocation.getCurrentPosition(callback);
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }
+    else {
+        callback(loc);
     }
 }
 
 function myOnLoad(){
     console.log("Starting OnLoad");
+
+    var today = Date.now();
+
+    var timeText = document.getElementById("dateTimeText");
+
+    timeText.innerHTML = new Date().toLocaleString();
+
+    //today = Date.UTC(2019,06,16,16);
+
+    //30 days between 1970 and 2000
+    var daysJ2000 = (today / 1000/365.25/24/3600 - 30) * 365.25;
+
+    console.log(daysJ2000);
+
+    var sunLongitude = 280.461 + 0.9856474*daysJ2000;
+
+    sunLongitude = degToRad(sunLongitude % 360);
+
+    var anomaly = 357.528 + 0.9856003 * daysJ2000;
+
+    anomaly = degToRad(anomaly % 360);
+
+    var eclipticLongitude = sunLongitude + degToRad(1.915 * Math.sin(anomaly) + 0.020 * Math.sin(2*anomaly));
+
+    var eclipticObliquity = degToRad(23.439 - 0.0000004*daysJ2000);
+
+    var y = Math.cos(eclipticObliquity) * Math.sin(eclipticLongitude);
+
+    var x = Math.cos(eclipticLongitude);
+
+    var RA = Math.atan2(y,x);
+
+    var printRA = rightAscensionFromRad(RA);
+
+    var DEC = radToDeg(Math.asin(Math.sin(eclipticObliquity) * Math.sin(eclipticLongitude)).toFixed(3));
+
+    console.log(printRA, DEC);
+
     getLocation(x =>
         {
     var latText = document.getElementById("locationLatitude");
@@ -78,7 +96,7 @@ function myOnLoad(){
     var zenRAText = document.getElementById("ZenithRA");
 
     zenDecText.innerHTML = x.coords.latitude.toFixed(2).toString();
-    zenRAText.innerHTML = rightAscensionFromRad(degToRad(greenwichSiderealTime2(x).toFixed(2).toString()));
+    zenRAText.innerHTML = rightAscensionFromRad(degToRad(greenwichSiderealTime2(x, daysJ2000).toFixed(2).toString()));
 
 
     var sunDecText = document.getElementById("sunDec");
@@ -90,7 +108,7 @@ function myOnLoad(){
 
     var angleText = document.getElementById("angleValue");
 
-    var angle = radToDeg(haversine(degToRad(x.coords.latitude), degToRad(greenwichSiderealTime2(x)*15), degToRad(DEC), RA));
+    var angle = radToDeg(haversine(degToRad(x.coords.latitude), degToRad(greenwichSiderealTime2(x, daysJ2000)*15), degToRad(DEC), RA));
     angleText.innerHTML = angle.toFixed(2);
 
     var shadowText = document.getElementById("outputHeight");
@@ -121,13 +139,13 @@ function myOnLoad(){
     console.log("Finished");
 }
 
-function greenwichSiderealTime2(location) {
+function greenwichSiderealTime2(location, daysJ2000) {
     var days0 = Math.floor(daysJ2000);
     var hours = (daysJ2000 - days0)*24;
     var centuries = daysJ2000 / 36525;
 
     var GMST = (6.697374558 + 0.06570982441908*days0  + 1.00273790935*hours +  0.000026*(centuries**2))%24 + 12;
-    console.log(GMST)
+    console.log(GMST);
 
     var lst = GMST + location.coords.longitude / 15;
 
